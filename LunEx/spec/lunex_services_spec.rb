749aa276aa864344e3ab543a143d01a6a6bc4c5f
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-09 17:58:25 -0700

    Refactoring the tests for lunex_services
    Co-authored-by: Pat Maddox <pat@rubysteps.com>

--- LunEx/spec/lunex_services_spec.rb
+++ LunEx/spec/lunex_services_spec.rb
@@ -2,25 +2,30 @@ require "lunex_services"
 
 describe LunexServices do
   describe "#current_price" do
+    SAFE_SUNSPOT_VALUE = 80
+
+    let(:my_service) { LunexServices.new }
+
+    before(:each) do
+      allow(my_service).to receive(:sleep)
+      allow(my_service).to receive(:rand).and_return(SAFE_SUNSPOT_VALUE)
+    end
+
     it "sleeps five seconds" do
-      my_service = LunexServices.new
-      allow(my_service).to receive(:raise)
       expect(my_service).to receive(:sleep).with(5)
       my_service.current_price("HE3")
     end
 
-    it "randomly raises 20% of the time" do
-      my_service = LunexServices.new
-      allow(my_service).to receive(:sleep)
-      expect(my_service).to receive(:rand).with(100).and_return(81)
+    it "randomly raises error 20% of the time" do
+      expect(my_service).to receive(:rand).with(100)
+        .and_return(SAFE_SUNSPOT_VALUE + 1)
       expect { my_service.current_price("HE3") }
         .to raise_error("Sorry, sunspot activity today...please try again later")
     end
 
     it "returns a random price each time" do
-      my_service = LunexServices.new
-      allow(my_service).to receive(:sleep)
-      expect(my_service).to receive(:rand).and_return(1, 5, 1, 13)
+      expect(my_service).to receive(:rand)
+        .and_return(SAFE_SUNSPOT_VALUE, 5, SAFE_SUNSPOT_VALUE, 13)
       expect(my_service.current_price("HE3")).to equal(47)
       expect(my_service.current_price("HE3")).to equal(55)
     end
