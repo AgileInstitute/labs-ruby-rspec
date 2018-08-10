8e9521303ceedef40ba7d04d720374c96d14ea70
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-09 17:11:28 -0700

    next test
    Co-authored-by: Pat Maddox <pat@rubysteps.com>

--- LunEx/spec/lunex_services_spec.rb
+++ LunEx/spec/lunex_services_spec.rb
@@ -8,5 +8,13 @@ describe LunexServices do
       expect(my_service).to receive(:sleep).with(5)
       my_service.current_price("HE3")
     end
+
+    it "randomly raises 20% of the time" do
+      my_service = LunexServices.new
+      allow(my_service).to receive(:sleep)
+      expect(my_service).to receive(:rand).with(100).and_return(81)
+      expect { my_service.current_price("HE3") }
+        .to raise_error("Sorry, sunspot activity today...please try again later")
+    end
   end
 end
