343611c5c3ea032eae87babcdc1675c550ebf995
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-09 17:33:01 -0700

    Another test
    Co-authored-by: Pat Maddox <pat@rubysteps.com>

--- LunEx/spec/lunex_services_spec.rb
+++ LunEx/spec/lunex_services_spec.rb
@@ -16,5 +16,13 @@ describe LunexServices do
       expect { my_service.current_price("HE3") }
         .to raise_error("Sorry, sunspot activity today...please try again later")
     end
+
+    it "returns a random price each time" do
+      my_service = LunexServices.new
+      allow(my_service).to receive(:sleep)
+      expect(my_service).to receive(:rand).and_return(1, 5, 1, 13)
+      expect(my_service.current_price("HE3")).to equal(47)
+      expect(my_service.current_price("HE3")).to equal(55)
+    end
   end
 end
