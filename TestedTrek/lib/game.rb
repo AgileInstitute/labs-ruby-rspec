b96f5436f8ef79d6f948afa93f62bddf95479f3f
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-12 17:02:38 -0700

    more working char tests in TestedTrek

--- TestedTrek/lib/game.rb
+++ TestedTrek/lib/game.rb
@@ -4,53 +4,46 @@ class Game
   MAX_PHASER_RANGE = 4000
 
   attr_reader :e
-  #     this.t = 8;
+  #     self.t = 8;
 
   def initialize
     @e = 10_000
   end
-#     generator: function() do
-#         return Math.random();
-#    end,
-#     randomWithinLimitOf: function(n) do
-#         return Math.floor(this.generator() * n);
-#    end,
 
   def fire_weapon(wg)
 #         if(wg.parameter("command") == "phaser") do
-            amount = wg.parameter("amount").to_i
-            enemy = wg.variable("target");
-            if @e >= amount
-                distance = enemy.distance;
-                if distance > Game::MAX_PHASER_RANGE
-                    wg.write_line("Klingon out of range of phasers at #{distance} sectors...");
-#                end else do
-#                     damage = amount - (((amount / 20) * distance / 200) + this.randomWithinLimitOf(200));
-#                     if (damage < 1) do
-#                         damage = 1;
-#                    end
-#                     wg.write_line("Phasers hit Klingon at " + distance + " sectors with " + damage + " units");
-#                     if (damage < enemy.energy) do
-#                         enemy.energy = enemy.energy - damage;
-#                         wg.write_line("Klingon has " + enemy.energy + " remaining");
-#                    end else do
-#                         wg.write_line("Klingon destroyed!");
-#                         enemy.destroy();
-#                    end
-#                end
-                @e -= amount;
-                end
-else
-                wg.write_line("Insufficient energy to fire phasers!")
+    amount = wg.parameter("amount").to_i
+    enemy = wg.variable("target")
+    if @e >= amount
+      distance = enemy.distance
+      if distance > Game::MAX_PHASER_RANGE
+        wg.write_line("Klingon out of range of phasers at #{distance} sectors...")
+      else
+        damage = amount - (((amount / 20) * distance / 200) + rand(200))
+        if damage < 1
+          damage = 1
+        end
+        wg.write_line("Phasers hit Klingon at #{distance} sectors with #{damage} units")
+        if (damage < enemy.energy)
+          enemy.energy -= damage
+          wg.write_line("Klingon has #{enemy.energy} remaining")
+        else
+          wg.write_line("Klingon destroyed!");
+          enemy.destroy();
+        end
+      end
+      @e -= amount;
+    else
+      wg.write_line("Insufficient energy to fire phasers!")
 #            end
 #        end else if(wg.parameter("command") == "photon") do
 #             enemy = wg.variable("target");
-#             if(this.t > 0) do
+#             if(self.t > 0) do
 #                 distance = enemy.distance;
-#                 if ((this.randomWithinLimitOf(4) + ((distance / 500) + 1) > 7)) do
+#                 if ((self.randomWithinLimitOf(4) + ((distance / 500) + 1) > 7)) do
 #                     wg.write_line("Torpedo missed Klingon at " + distance + " sectors...");
 #                end else do
-#                     damage = 800 + this.randomWithinLimitOf(50);
+#                     damage = 800 + self.randomWithinLimitOf(50);
 #                     wg.write_line("Photons hit Klingon at " + distance + " sectors with " + damage + " units");
 #                     if (damage < enemy.energy) do
 #                         enemy.energy = enemy.energy - damage;
@@ -60,7 +53,7 @@ else
 #                         enemy.destroy();
 #                    end
 #                end
-#                 this.t--;
+#                 self.t--;
 #            end else do
 #                 wg.write_line("No more photon torpedoes!");
 #            end
--- TestedTrek/spec/phaser_characterization_spec.rb
+++ TestedTrek/spec/phaser_characterization_spec.rb
@@ -43,35 +43,37 @@ describe "phasers" do
             expect(@game.e).to equal(@energy_before - energy_to_fire)
         end
     end
-#
-#     describe "when Klingon destroyed by sufficient strike" do
-#         var klingon
-#         before(:each) do
-#             klingon = new Klingon(2000, 200)
-#             spyOn(klingon, "destroy")
-#             wg.target = klingon
-#             wg.commandParameter = 1000
-#             spyOn(game, "generator").and.returnValue(0)
-#
-#             game.fire_weapon(wg)
-#         end
-#
-#         it "reports hit and destroyed" do
-#             expect(@wg).to have_received(:write_line)
-  # .with("Phasers hit Klingon at 2000 sectors with 500 units")
-#             expect(@wg).to have_received(:write_line)
-  # .with("Klingon destroyed!")
-#         end
-#
-#         it "subtracts energy" do
-#             expect(game.e).toBe(energyBefore - 1000)
-#         end
-#
-#         it "really destroys" do
-#             expect(klingon.destroy).toHaveBeenCalled()
-#         end
-#     end
-#
+
+    describe "when Klingon destroyed by sufficient strike" do
+        before(:each) do
+          @klingon = Klingon.new(2000, 200)
+          allow(@wg).to receive(:variable).with("target")
+            .and_return(@klingon)
+
+          allow(@klingon).to receive(:destroy)
+          allow(@wg).to receive(:parameter).and_return(1000)
+          expect(@game).to receive(:rand)
+            .and_return(0)
+
+          @game.fire_weapon(@wg)
+        end
+
+        it "reports hit and destroyed" do
+            expect(@wg).to have_received(:write_line)
+  .with("Phasers hit Klingon at 2000 sectors with 500 units")
+            expect(@wg).to have_received(:write_line)
+  .with("Klingon destroyed!")
+        end
+
+        it "subtracts energy" do
+            expect(@game.e).to equal(@energy_before - 1000)
+        end
+
+        it "really calls destroy" do
+            expect(@klingon).to have_received(:destroy)
+        end
+    end
+
 #     describe "when damaging Klingon" do
 #         before(:each) do
 #             wg.target = new Klingon(2000, 200)
