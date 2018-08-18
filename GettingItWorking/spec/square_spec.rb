02eb2ee69a0b20af7a5dc0e43a837e8aca7278b8
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-17 18:05:12 -0700

    broke the GettingItWorking test into multiple tests and a let() initializer.

--- GettingItWorking/spec/square_spec.rb
+++ GettingItWorking/spec/square_spec.rb
@@ -1,9 +1,13 @@
 require 'square'
 
 describe "square function" do
-  it "works for trivial cases" do
-    maths = MyMath.new
+  let(:maths) { MyMath.new }
+
+  it "squares two" do
     expect(maths.square(2)).to eq(4)
+  end
+
+  it "squares three" do
     expect(maths.square(3)).to eq(9)
   end
 end
